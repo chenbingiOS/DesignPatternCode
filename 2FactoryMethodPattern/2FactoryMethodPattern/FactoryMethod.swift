43db928184499cd0a2f7393b7c544cb08ea732fb
chenbingiOS <chenbingios@sina.com>
2021-11-04 15:04:33 +0800

    【Feature】工厂方法

---
+++ 2FactoryMethodPattern/2FactoryMethodPattern/FactoryMethod.swift
@@ -0,0 +1,10 @@
+//
+//  FactoryMethod.swift
+//  2FactoryMethodPattern
+//
+//  Created by mtAdmin on 2021/11/4.
+//
+
+import Foundation
+
+/// 工厂方法是一种创建型设计模式， 解决了在不指定具体类的情况下创建产品对象的问题。
---
+++ 2FactoryMethodPattern/2FactoryMethodPatternTests/_FactoryMethodPatternTests.swift
@@ -0,0 +1,33 @@
+//
+//  _FactoryMethodPatternTests.swift
+//  2FactoryMethodPatternTests
+//
+//  Created by mtAdmin on 2021/11/4.
+//
+
+import XCTest
+@testable import _FactoryMethodPattern
+
+class _FactoryMethodPatternTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
